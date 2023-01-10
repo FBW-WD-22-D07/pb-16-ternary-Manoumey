a0914180fcaf0bedb7019beaa28d78a935f5aee8
Manoumey <manuella.meyer@dci.education>
2023-01-10 14:43:10 +0100

    2. commit

--- solution.js
+++ solution.js
@@ -46,6 +46,10 @@ console.log(okMarie === "cake" ? `Let them eat cake` : `Oh, bother`);
 // * num3 = 40.9
 
 
-const num = 939;
+const num1 = 30;
+const num2 = 939;
+const num3 = 40.9;
 
-console.log(num % 2 == 0 ? `${num} is even` : `${num} is odd` );
+console.log(num1 % 2 == 0 ? `${num1} is even` : `${num1} is odd` );
+console.log(num2 % 2 == 0 ? `${num2} is even` : `${num2} is odd` );
+console.log(num3 % 2 == 0 ? `${num3} is even` : `${num3} is odd` );
